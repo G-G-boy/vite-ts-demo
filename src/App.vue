2ffb027344d17b5966b3c6a34291ed1f4f7b0aa0
Gavin.guo <Gavin.guo@dfox.cn>
2020-07-23 00:41:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <HelloWorld msg="Hello Vue 3.0 + Vite"/>
+</template>
+
+<script lang="ts">
+    import HelloWorld from "./components/HelloWorld.vue";
+
+    export default {
+        name: "App",
+        components: {
+            HelloWorld
+        },
+        setup(props: any, ctx: any) {
+
+        }
+    };
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1 class="title">{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+    import {ref} from "vue";
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        setup(props: any) {
+            const count = ref<number>(0);
+
+            return {
+                count
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .title {
+    color: aquamarine;
+  }
+</style>
