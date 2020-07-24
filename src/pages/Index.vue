aaaed07341af142dc00d52ade4ea3e833ad29d80
Gavin.guo <Gavin.guo@dfox.cn>
2020-07-25 01:10:50 +0800

    使用composition-api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,8 @@
     <div>
         <button @click="add">add</button>
         <h1>count:{{count}}</h1>
+        <button @click="changeTime">change Time</button>
+        <h2>time: {{time}}</h2>
     </div>
     <div>
         <h1>count2.a:{{count2.a}}</h1>
@@ -20,24 +22,33 @@
 </template>
 
 <script lang="ts">
-    import {ref, reactive, toRefs, computed, watch} from "vue";
+    import {ref, reactive, toRefs, computed, watch, watchEffect} from "vue";
     export default {
         name: "Index",
         setup() {
             const count = ref<number>(0);
-            const count2 = ref({a: 1});
-            const count3 = reactive({
+            const count2 = ref<{a: number}>({a: 1});
+            const count3 = reactive<{b: number, c: number}>({
                 b: 2,
                 c: 3
             });
+
+            const time = ref<number>(new Date().getTime());
             const add = () => {
                 count.value++;
                 count2.value.a++;
                 count3.b++;
                 count3.c++;
             }
-            watch(() => count.value, () => {
-                console.log(count.value,'------------------------')
+
+            const changeTime = () => {
+                time.value = new Date().getTime();
+            }
+            watch(count, () => {
+                console.log('count的值改变了-----',count.value);
+            });
+            watchEffect(() => {
+                console.log('依赖变化了----', time.value, '--------',count.value);
             });
             return {
                 count,
@@ -45,7 +56,9 @@
                 count2,
                 count3,
                 ...toRefs(count3),
-                count4: computed(() => count.value**2)
+                count4: computed(() => count.value**2),
+                time,
+                changeTime
             }
         }
     }
