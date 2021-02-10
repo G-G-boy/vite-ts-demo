e0489880f75cb852828534c329592976237cca2e
Gavin.guo <Gavin.guo@dfox.cn>
2021-02-11 01:01:51 +0800

    添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import {defineConfig} from 'vite';
 import path from 'path';
 
-export default defineConfig({
-    plugins: [vue()],
+import {viteMockServe} from 'vite-plugin-mock';
+
+export default defineConfig(({command}) => ({
+    plugins: [vue(), viteMockServe({mockPath: 'mock', localEnabled: command === 'serve'})],
     alias: {
         '@components': path.resolve(__dirname, 'src/components'),
     },
-});
+}));
