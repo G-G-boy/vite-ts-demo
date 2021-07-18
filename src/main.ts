e81de87b56458a932192c0cbb21906ee5ee944a6
Gavin.guo <Gavin.guo@dfox.cn>
2021-07-18 16:51:23 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
 import store from './store';
 import App from './App.vue';
 import Index from './pages/Index.vue';
-import Layout from './pages/Layout.vue';
-import One from './pages/One.vue';
-import Two from './pages/Two.vue';
-import Three from './pages/Three.vue';
 import 'ant-design-vue/dist/antd.css';
 import 'vant/lib/index.css';
 import './index.css';
@@ -14,50 +10,12 @@ import './index.css';
 const routes: RouteRecordRaw[] = [
     {
         path: '',
-        redirect: '/two',
+        redirect: '/home',
     },
     {
         path: '/index',
         name: 'Index',
         component: Index,
-    },
-    {
-        path: '/layout',
-        name: 'Layout',
-        component: Layout,
-        children: [
-            {
-                path: '',
-                component: One,
-            },
-            {
-                path: 'one',
-                component: One,
-            },
-            {
-                path: 'two',
-                component: Two,
-            },
-            {
-                path: 'three',
-                component: Three,
-            },
-        ],
-    },
-    {
-        path: '/one',
-        name: 'One',
-        component: One,
-    },
-    {
-        path: '/two',
-        name: 'Two',
-        component: Two,
-    },
-    {
-        path: '/three',
-        name: 'Three',
-        component: Three,
     },
     {
         path: '/home',
