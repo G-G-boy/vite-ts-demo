f6bcd4704053dd1cc55438c1c599df23058a649f
Gavin.guo <Gavin.guo@dfox.cn>
2021-09-21 14:40:36 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import vue from '@vitejs/plugin-vue';
 import {defineConfig} from 'vite';
 import path from 'path';
 import ViteComponents, {VantResolver} from 'vite-plugin-components';
-import {viteMockServe} from 'vite-plugin-mock';
 
-export default defineConfig(({command}) => ({
+export default defineConfig(() => ({
     plugins: [
         vue(),
-        viteMockServe({mockPath: 'mock', localEnabled: command === 'serve'}),
         ViteComponents({
             customComponentResolvers: [VantResolver()],
         }),
